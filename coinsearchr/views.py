e939d4b1f5b0407e943b879cacd930d7e455e064
CoinSearchr <93884776+CoinSearchr@users.noreply.github.com>
2021-11-09 03:05:23 -0500

    Make the search results page passible

--- coinsearchr/views.py
+++ coinsearchr/views.py
@@ -4,27 +4,66 @@ from . import app
 from . import searcher
 from . import db
 
+from math import log10, floor, isnan
+import re
+
+@app.template_filter()
+def format_currency_num(num: float) -> str:
+	""" Formats numbers nicely of all orders of magnitude. """
+	def round_sig(x, sig=2):
+		if x<=0 or not x or isnan(x):
+			return x
+		return round(x, sig-int(floor(log10(abs(x))))-1)
+
+	def remove_dot0(num: float) -> str:
+		num = str(num)
+		if num.endswith('.0'):
+			return num[:-2]
+		return num
+
+	num = round_sig(num, 3)
+	if num > 1e12:
+		return f'{remove_dot0(num/1e12)} T'
+	if num > 1e9:
+		return f'{remove_dot0(num/1e9)} B'
+	if num > 1e6:
+		return f'{remove_dot0(num/1e6)} M'
+	if num > 1e3:
+		return f'{remove_dot0(num/1e3)} k'
+	return remove_dot0(num)
+
+@app.template_filter()
+def time_ago(date_time) -> str:
+	return "3 minutes ago"
+
 @app.route('/')
 def index():
 	return render_template("index.jinja2")
 
 @app.route('/search', methods=['GET']) # args: q=search_term, json=true, currency=usd+, 
 def search():
-    arg_search_term = request.args.get('q', '')
-    arg_currency = request.args.get('currency', 'usd').lower()
+	arg_search_term = request.args.get('q', '')
+	arg_currency = request.args.get('currency', 'usd').lower()
 
-    df = searcher.search_in_database_ranked(arg_search_term, arg_currency)
-    # TODO check if currency is valid, default if it's not valid
+	df = searcher.search_in_database_ranked(arg_search_term, arg_currency)
+	# TODO check if currency is valid, default if it's not valid
 
-    data = {
-        'show_plus_on_result_count': '+' if len(df.index) >= db.max_query_results else '',
-        'search_term': arg_search_term,
-        'currency': arg_currency,
-    }
+	data = {
+		'show_plus_on_result_count': '+' if len(df.index) >= db.max_query_results else '',
+		'search_term': arg_search_term,
+		'currency': arg_currency,
+		'units_prefix': '',
+		'units_suffix': '',
+	}
 
-    if request.args.get('json', 'false').lower() == 'true':
-        return jsonify(df.to_dict('records'))
+	if arg_currency == 'usd':
+		data['units_prefix'] = '$'
+	else:
+		data['units_suffix'] = arg_currency.upper()
 
-    else:
-        return render_template("search_results.jinja2", df=df, data=data)
-        
+	if request.args.get('json', 'false').lower() == 'true':
+		return jsonify(df.to_dict('records'))
+
+	else:
+		return render_template("search_results.jinja2", df=df, data=data)
+		
