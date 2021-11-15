e1885a4d68dcca658c6f086a761facf2e00faac7
CoinSearchr <93884776+CoinSearchr@users.noreply.github.com>
2021-11-15 07:36:09 +0000

    Minor performance increase in DB search (#18)

--- coinsearchr/searcher.py
+++ coinsearchr/searcher.py
@@ -5,14 +5,26 @@ import pandas as pd
 
 
 from . import db
+from . import app
+
+import time
+import logging
+logger = logging.getLogger(__name__)
 
 sql_engine = db.create_engine()
 
 def search_in_database(search_term: str, currency: str = 'usd') -> pd.DataFrame:
 	""" Searches for a search term in the database. Returns all matching coins. """
+	stime = time.time()
 
 	df = pd.read_sql_query(
-		'SELECT * FROM coin_list_detail WHERE ((name LIKE ? OR symbol LIKE ?) AND base_currency = ?) ORDER BY market_cap DESC LIMIT ?',
+		"""
+		SELECT source, base_currency, id, symbol, name, image, current_price, market_cap, market_cap_rank, fully_diluted_valuation, 
+			total_volume, high_24h, low_24h, price_change_24h, price_change_percentage_24h, market_cap_change_24h, market_cap_change_percentage_24h,
+			circulating_supply, total_supply, max_supply, ath, ath_change_percentage, ath_date, atl, atl_change_percentage, atl_date, last_updated, 
+			price_change_percentage_1h_in_currency, price_change_percentage_24h_in_currency, price_change_percentage_7d_in_currency, date, page_num
+		FROM coin_list_detail WHERE ((name LIKE ? OR symbol LIKE ?) AND base_currency = ?)
+		ORDER BY market_cap DESC LIMIT ? """,
 		params = (
 			'%' + search_term + '%',
 			'%' + search_term + '%',
@@ -21,6 +33,8 @@ def search_in_database(search_term: str, currency: str = 'usd') -> pd.DataFrame:
 		),
 		con=sql_engine
 	) # TODO union with exact matches so we can later place them first
+
+	app.logger.info(f'Database query took {(time.time() - stime)*1000:.0f}ms.')
 
 	return df
 
